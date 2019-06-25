6b439a3f39e54b556b2ef4cda5503eebc54b087e
Joaquin Moya <joaquinmoyavargas@gmail.com>
2019-06-24 23:50:08 -0300

    No pude hacer una verga

---
+++ ShoppingBuyAll/Formularios/frm_ReporteProductos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoppingBuyAll.Formularios
+{
+    public partial class frm_ReporteProductos : Form
+    {
+        public frm_ReporteProductos()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_ReporteProductos_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
